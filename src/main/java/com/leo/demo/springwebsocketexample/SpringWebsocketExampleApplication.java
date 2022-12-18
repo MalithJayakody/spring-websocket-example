753cbc94e7d61b15e1a27e2276076a45cd23ad05
Malith Jayakody <malithjayakody@Maliths-MacBook-Air.local>
2022-12-18 11:24:43 +0530

    Init commit

---
+++ src/main/java/com/leo/demo/springwebsocketexample/SpringWebsocketExampleApplication.java
@@ -0,0 +1,13 @@
+package com.leo.demo.springwebsocketexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsocketExampleApplication.class, args);
+	}
+
+}
